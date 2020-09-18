fbaca2d6dde57d0c4f131605e413b8b35a91ef02
tarush-goel <70821023+tarush-goel@users.noreply.github.com>
2020-09-19 04:36:11 +0530

    Update two.py

---
+++ two.py
@@ -0,0 +1,14 @@
+"""
+print("Hello, world!")
+a,b,c,d,e = 28,1.5,"Hello",True,None
+name=input("Name: ")
+print("Hello, "+name)
+print(f"Hello, {name}")
+n=int(input("Number: "))
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is zero")
+"""
